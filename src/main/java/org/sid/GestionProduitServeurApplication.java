e763db789b9d2b703a6b3bd8b12362c0a68db56f
BelmbyPro <belmondotchinda@gmail.com>
2018-04-27 23:11:48 +0200

    first commit

---
+++ src/main/java/org/sid/GestionProduitServeurApplication.java
@@ -0,0 +1,12 @@
+package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProduitServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProduitServeurApplication.class, args);
+	}
+}
